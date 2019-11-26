237567a235fc0c1f5df6ac85d282892853efa495
unknown <richard.okoye@netopng.com>
2019-11-26 22:59:38 +0100

    first commit

---
+++ rossfairgrieve.com/wp-content/plugins/leaflet-maps-marker/leaflet-georss7cfb.php
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0" xmlns:georss="http://www.georss.org/georss" xmlns:gml="http://www.opengis.net/gml" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/">
+<channel>
+<atom:link href="https://rossfairgrieve.com/wp-content/plugins/leaflet-maps-marker/leaflet-georss.php?layer=all" rel="self" type="application/rss+xml" />
+<link>https://rossfairgrieve.com/wp-content/plugins/leaflet-maps-marker/leaflet-fullscreen.php?layer=0</link>
+<title>Ross Fairgrieve - Filmmaker, Cameraman &amp; Photographer - maps</title>
+<lastBuildDate>Thu, 01 Jan 1970 12:01:00 +0100</lastBuildDate>
+<generator>www.mapsmarker.com</generator>
+<description>GeoRSS-feed created with Maps Marker Pro (www.mapsmarker.com)</description>
+</channel>
+</rss>
